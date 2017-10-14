c57ce8835732addb1aa74534a981b751b55a649b
gazeatmyshoes <32770701+gazeatmyshoes@users.noreply.github.com>
2017-10-14 13:49:37 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+class SSLWSGIRefServer(bottle.ServerAdapter):
+    def run(self, handler):
+        from wsgiref.simple_server import make_server, WSGIRequestHandler
+        import ssl
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw): pass
+            self.options['handler_class'] = QuietHandler
+        srv = make_server(self.host, self.port, handler, **self.options)
+        srv.socket = ssl.wrap_socket (srv.socket,
+        certfile='server.pem', server_side=True)
+        srv.serve_forever()
